b5067d22746cb3c4d816c12ac18db198573204ca
Many Diamond <many.pichr168@gmail.com>
2022-07-24 15:23:55 +0700

    fix deploy

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-import models from './models';
-import routes from './routes';
+import models from './src/models';
+import routes from './src/routes';
 
 const app = express();
 
